06044b645388869ef32ae7775197a6c7a2c3e7a9
Varun Kumar <varu1311@gmail.com>
2023-03-17 11:25:22 +0530

    chore: remove comments

--- src/services/project.service.js
+++ src/services/project.service.js
@@ -13,16 +13,16 @@ const { zipFolder } = require('./zipping.service');
 
 const generateProject = async (data) =>{
   const {services} = data;
-  // const projectResult = await projectRepository.create(
+  const projectResult = await projectRepository.create(
       
-  //   {userId:'de7405f2-f2b5-4c8d-95f9-7c8beb3e5023'}
+    {userId:'de7405f2-f2b5-4c8d-95f9-7c8beb3e5023'}
         
-  // );
-  // const projectId =  projectResult.id;
-  // services.forEach(async (service)=>{
-  //   repositoryServiceObj[service.service_type](service,projectId);
-  // });
-  const projectId = '6d5ef481-7340-44c0-991c-df95714d76ac';
+  );
+  const projectId =  projectResult.id;
+  services.forEach(async (service)=>{
+    repositoryServiceObj[service.service_type](service,projectId);
+  });
+  // const projectId = '6d5ef481-7340-44c0-991c-df95714d76ac';
   const configurations = getConfigurations(services);
   console.log(configurations);
   let generatorResponses = [];
