76543dc19bad7bab7f91da2261c375234ba87f76
apoorvGit <apoorvmishra1000@gmail.com>
2023-03-21 11:35:13 +0530

    feat: add controller to get latest project services

--- src/controllers/project.controller.js
+++ src/controllers/project.controller.js
@@ -14,5 +14,15 @@ const generateProjectController = async (req,res)=>{
   }
 
 };
-
-module.exports = {generateProjectController};
+const getLatestProjectController = async (req,res)=>{
+  try{
+    const token = req.headers.authorization.split(' ')[1];
+    const decoded = await jwt.verify(token, process.env.JWT_SECRET);
+    const project = await ProjectService.getLatestProject(decoded.id);
+    res.status(200).json(project);
+  }catch (err) {
+    console.log(err);
+    res.status(500).json({data:err.message});
+  }
+};
+module.exports = {generateProjectController, getLatestProjectController};
