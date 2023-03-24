39a7f9d4afdb11e21b4c4b7e5ea1112f46b083f4
Varun Kumar <varu1311@gmail.com>
2023-03-24 12:21:52 +0530

    feat: change image pull policy

--- _templates/database/many/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  params: ({ args }) => {
-    const parsedConfig = JSON.parse(args.config);
-    // console.log('parsedConfig', parsedConfig.database);
-    return parsedConfig.database;
-  }    
-};
