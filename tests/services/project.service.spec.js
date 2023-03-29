10ccc36c60fc3b68b5b11a93849e9f615898ad2f
Varun Kumar <varu1311@gmail.com>
2023-03-29 18:00:57 +0530

    chore: update tests

--- tests/services/project.service.spec.js
+++ tests/services/project.service.spec.js
@@ -48,7 +48,34 @@ jest.mock('../../src/services/zipping.service.js', () => {
     zipFolder: jest.fn(),
   };
 });
-
+const mockreq = {body:{'services':[
+  {
+    'service_type': 'FrontEnd',
+    'configurations':{
+      'reactVersion': '2.08',
+      'port':'5432',
+      'numberOfReplicas':5,
+      'name':'React Todo App'
+    },
+    'customEnv':{
+      'field': 'port',
+      'value':'2345'
+    }
+  },
+  {
+    'service_type': 'BackEnd',
+    'configurations':{
+      'reactVersion': '2.08',
+      'port':'5432',
+      'numberOfReplicas':5,
+      'name':'React Todo App'
+    },
+    'customEnv':{
+      'field': 'port',
+      'value':'2345'
+    }
+  }
+]}};
 describe('microservices service testing', () => {
   beforeEach(() => {
     jest.resetModules();
@@ -64,34 +91,6 @@ describe('microservices service testing', () => {
     jest.spyOn(repositoryServiceObj,'BackEnd').mockResolvedValueOnce({
       'id':4,
     });
-    const mockreq = {body:{'services':[
-      {
-        'service_type': 'FrontEnd',
-        'configurations':{
-          'reactVersion': '2.08',
-          'port':'5432',
-          'numberOfReplicas':5,
-          'name':'React Todo App'
-        },
-        'customEnv':{
-          'field': 'port',
-          'value':'2345'
-        }
-      },
-      {
-        'service_type': 'BackEnd',
-        'configurations':{
-          'reactVersion': '2.08',
-          'port':'5432',
-          'numberOfReplicas':5,
-          'name':'React Todo App'
-        },
-        'customEnv':{
-          'field': 'port',
-          'value':'2345'
-        }
-      }
-    ]}};
     // eslint-disable-next-line no-unused-vars
     const mockres = {
       status: jest.fn().mockReturnThis(),
@@ -102,6 +101,17 @@ describe('microservices service testing', () => {
   });
   it('should call the generators with correct parameters', async () => {
     process.env.OFFLINE_ENABLED = 'false';
+    jest.spyOn(projectRepository,'create').mockResolvedValueOnce({
+      'id':projectId,
+      'userId':4
+    });
+    jest.spyOn(repositoryServiceObj,'FrontEnd').mockResolvedValueOnce({
+      'id':3,
+    });
+    jest.spyOn(repositoryServiceObj,'BackEnd').mockResolvedValueOnce({
+      'id':4,
+    });
+    await services.generateProject(mockreq.body);
     const folderPath = path.join(OUTPUT_PATH, projectId.toString());
     const zipPath = path.join(OUTPUT_PATH, `${projectId}.zip`);
     const projectDir = path.join(OUTPUT_PATH, projectId.toString());
