b06701c05a57beadb875f165f9aedbeaf4f67296
apoorvGit <apoorvmishra1000@gmail.com>
2023-04-04 12:54:19 +0530

    chores: add jest config file to ignore repos, configs and exceptions in test coverage

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  collectCoverageFrom: [
+    'src/**/*.js',
+    '!src/repositories/**/*.js',
+    '!src/config/**',
+    '!src/routes/**',
+    '!src/exceptions/**',
+    '!src/schemas/**',
+  ]
+};
+  
